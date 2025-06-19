a7735331603a2bc4ae756e214f14f80e69c4b5c0
Jon Basarte <jon.basarte@alumni.mondragon.edu>
2025-06-19 19:03:44 +0200

    estoy picando

--- DC.R
+++ DC.R
@@ -16,7 +16,7 @@ df=read.csv("C:/Users/jonba/Downloads/water-quality-data.csv")
 
 vis_miss(df,warn_large_data = FALSE)
 
-df_sample <- df[sample(1:nrow(df), size = 1000), ]
+df_sample <- df[sample(1:nrow(df), size = 10000), ]
 df_2 <- kNN(df_sample, 
             variable = c("Value", "MDL", "RDL"), 
             dist_var = c("Area", "Site.Type"), 
@@ -24,6 +24,6 @@ df_2 <- kNN(df_sample,
             imp_var = FALSE)
 vis_miss(df_2)
 
-write.csv(df2, "DF_DC_na.csv")
+write.csv(df_2, "DF_DC_na.csv")
 
 
