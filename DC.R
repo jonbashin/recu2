f380f06bd5a322b7d3d6f68d4496a2eeb9b14dff
Jon Basarte <jon.basarte@alumni.mondragon.edu>
2025-06-19 18:46:53 +0200

    final de DD

---
+++ DC.R
@@ -0,0 +1,25 @@
+library(digest)
+library(discretization)
+library(dplyr)
+library(EnvStats)
+library(fastDummies)
+library(naniar)
+library(nortest)
+library(readxl)
+library(smoothmest)
+library(VIM)
+library(plumber)
+
+
+
+df=read.csv("C:/Users/jonba/Downloads/water-quality-data.csv")
+
+vis_miss(df,warn_large_data = FALSE)
+
+df_sample <- df[sample(1:nrow(df), size = 1000), ]
+df_2 <- kNN(df_sample, 
+            variable = c("Value", "MDL", "RDL"), 
+            dist_var = c("Area", "Site.Type"), 
+            k = 5, 
+            imp_var = FALSE)
+vis_miss(df_2)
